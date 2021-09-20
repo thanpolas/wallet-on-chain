3f7cd129e7d85c4c6a0e3dd3c9a8748cff6d4b12
Thanos Polychronakis <thanpolas@gmail.com>
2021-09-20 12:03:12 +0300

    test event emmition on receive

--- test/app/wallet-test.js
+++ test/app/wallet-test.js
@@ -35,8 +35,33 @@ describe('Wallet', function () {
 
     const balance = await wallet.getBalance();
     const compareTo = ethers.BigNumber.from('1000000000000000000');
-    console.log('balance:', balance.toString(), compareTo.toString());
 
     expect(ethers.BigNumber.from(balance).eq(compareTo)).to.equal(true);
+  });
+  it('Should emit "Received" event when receiving eth', async function () {
+    const [owner] = await ethers.getSigners();
+
+    const Wallet = await ethers.getContractFactory('Wallet');
+    const wallet = await Wallet.deploy(owner.address, 500000000);
+    await wallet.deployed();
+    const compareTo = ethers.BigNumber.from('1000000000000000000');
+
+    const endPromise = new Promise((resolve) => {
+      wallet.once('Received', (sender, value) => {
+        expect(ethers.BigNumber.from(value).eq(compareTo)).to.equal(true);
+        expect(sender).to.equal(owner.address);
+        resolve();
+      });
+    });
+    const transactionHash = await owner.sendTransaction({
+      to: wallet.address,
+      value: ethers.utils.parseEther('1.0'), // Sends exactly 1.0 ether
+    });
+
+    const balance = await wallet.getBalance();
+
+    expect(ethers.BigNumber.from(balance).eq(compareTo)).to.equal(true);
+
+    return endPromise;
   });
 });
