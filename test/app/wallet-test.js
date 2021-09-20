bb7c8347400ed5403f05af8e2797fe425faea7b8
Thanos Polychronakis <thanpolas@gmail.com>
2021-09-20 12:51:38 +0300

    simplify wallet tests

--- test/app/wallet-test.js
+++ test/app/wallet-test.js
@@ -2,66 +2,69 @@ const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
 describe('Wallet', function () {
-  it('Should deploy and confirm owner of contract', async function () {
-    const [owner, addr1] = await ethers.getSigners();
+  describe('Surface Tests', function () {
+    beforeEach(async function () {
+      const [owner] = await ethers.getSigners();
 
-    const Wallet = await ethers.getContractFactory('Wallet');
-    const wallet = await Wallet.deploy(owner.address, 500000000);
-    await wallet.deployed();
+      const Wallet = await ethers.getContractFactory('Wallet');
+      const wallet = await Wallet.deploy(owner.address, 500000000);
+      await wallet.deployed();
 
-    expect(await wallet.owner()).to.equal(owner.address);
-  });
-  it('Should deploy and have zero balance', async function () {
-    const [owner, addr1] = await ethers.getSigners();
-
-    const Wallet = await ethers.getContractFactory('Wallet');
-    const wallet = await Wallet.deploy(owner.address, 500000000);
-    await wallet.deployed();
-
-    expect(await wallet.getBalance()).to.equal(0);
-  });
-
-  it('Should deploy and have 1 eth balance', async function () {
-    const [owner] = await ethers.getSigners();
-
-    const Wallet = await ethers.getContractFactory('Wallet');
-    const wallet = await Wallet.deploy(owner.address, 500000000);
-    await wallet.deployed();
-
-    const transactionHash = await owner.sendTransaction({
-      to: wallet.address,
-      value: ethers.utils.parseEther('1.0'), // Sends exactly 1.0 ether
+      this.wallet = wallet;
+      this.owner = owner;
+    });
+    it('Should deploy and confirm owner of contract', async function () {
+      expect(await this.wallet.owner()).to.equal(this.owner.address);
+    });
+    it('Should deploy and have zero balance', async function () {
+      expect(await this.wallet.getBalance()).to.equal(0);
     });
 
-    const balance = await wallet.getBalance();
-    const compareTo = ethers.BigNumber.from('1000000000000000000');
-
-    expect(ethers.BigNumber.from(balance).eq(compareTo)).to.equal(true);
-  });
-  it('Should emit "Received" event when receiving eth', async function () {
-    const [owner] = await ethers.getSigners();
-
-    const Wallet = await ethers.getContractFactory('Wallet');
-    const wallet = await Wallet.deploy(owner.address, 500000000);
-    await wallet.deployed();
-    const compareTo = ethers.BigNumber.from('1000000000000000000');
-
-    const endPromise = new Promise((resolve) => {
-      wallet.once('Received', (sender, value) => {
-        expect(ethers.BigNumber.from(value).eq(compareTo)).to.equal(true);
-        expect(sender).to.equal(owner.address);
-        resolve();
+    it('Should deploy and have 1 eth balance', async function () {
+      await this.owner.sendTransaction({
+        to: this.wallet.address,
+        value: ethers.utils.parseEther('1.0'), // Sends exactly 1.0 ether
       });
+
+      const balance = await this.wallet.getBalance();
+      const compareTo = ethers.BigNumber.from('1000000000000000000');
+
+      expect(ethers.BigNumber.from(balance).eq(compareTo)).to.equal(true);
     });
-    const transactionHash = await owner.sendTransaction({
-      to: wallet.address,
-      value: ethers.utils.parseEther('1.0'), // Sends exactly 1.0 ether
+    it('Should emit "Received" event when receiving eth', async function () {
+      const compareTo = ethers.BigNumber.from('1000000000000000000');
+
+      const endPromise = new Promise((resolve) => {
+        this.wallet.once('Received', (sender, value) => {
+          expect(ethers.BigNumber.from(value).eq(compareTo)).to.equal(true);
+          expect(sender).to.equal(this.owner.address);
+          resolve();
+        });
+      });
+      await this.owner.sendTransaction({
+        to: this.wallet.address,
+        value: ethers.utils.parseEther('1.0'), // Sends exactly 1.0 ether
+      });
+
+      const balance = await this.wallet.getBalance();
+
+      expect(ethers.BigNumber.from(balance).eq(compareTo)).to.equal(true);
+
+      return endPromise;
     });
+  });
+  describe('getAllowance() testing', function () {
+    beforeEach(async function () {
+      const [owner] = await ethers.getSigners();
 
-    const balance = await wallet.getBalance();
-
-    expect(ethers.BigNumber.from(balance).eq(compareTo)).to.equal(true);
-
-    return endPromise;
+      const Wallet = await ethers.getContractFactory('Wallet');
+      const wallet = await Wallet.deploy(owner.address, 500000000);
+      await wallet.deployed();
+      this.wallet = wallet;
+    });
+    it.only('will send daily allowance', async function () {
+      const res = await this.wallet.getAllowance();
+      console.log('res:', res);
+    });
   });
 });
